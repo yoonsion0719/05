4d8dde19c7413409b5059b6c300b9fda51fcb6e4
yoonsion0719 <yoonsonia0719@gmail.com>
2023-10-08 15:55:02 +0900

    05-6

--- main.c
+++ main.c
@@ -5,17 +5,31 @@
 
 int main(int argc, char *argv[]) {
 		
-	int n,i;
-	int sum=0;
-	
-	printf("input a number : ");
-	scanf("%i", &n);
-	
-	for (i=0; i<=n; i++)
-		sum+=i;
+	int answer=59;
+	int n;
+	int trial=0;
 	
 	
-	printf("the result is %i", sum);
+	do
+	{
+		//답을 입력받는다
+		printf("Guess a number :");
+		scanf("%i", &n); 
+		
+		//정답과 큰지/작은지 여부를 판단하고 메세지 출력
+		if (n>answer)
+		 printf("high!\n");
+		else if (n<answer)
+		 printf("low!\n"); 
+		 
+		trial+=1;
+		
+	}
+		
+	while (n!=answer); //정답과 동일하면 루프에서 빠져나옴 
+	
+	printf("Congratulation! trials: %i", trial);
+
 	
 	
 	return 0;
